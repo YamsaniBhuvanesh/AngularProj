fbc87b3c0355fdf9b4f1e64ccb19b0b836969d4e
YamsaniBhuvanesh <yamsanibhuvanesh1@gmail.com>
2025-09-15 23:03:10 +0530

    created models and environment files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', redirectTo: '/products' , pathMatch:'full'
+  },
+  { path: 'products', component : ProductListComponent}
+];
 
 @NgModule({
   imports: [CommonModule,RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ProductModule } from './product/product.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule // Rely on AppRoutingModule for routing
+    AppRoutingModule, // Rely on AppRoutingModule for routing
+    ProductModule
   ],
   providers: [
-    provideHttpClient() // Provide HTTP client services
+    provideHttpClient(),
+    provideAnimationsAsync() // Provide HTTP client services
   ],
   bootstrap: [AppComponent]
 })
